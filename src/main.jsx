fda144d8c93cb4b196adb7fbb694963b8dee595f
gitahievans <gitahievance@gmail.com>
2023-06-14 23:12:06 +0300

    Create cart context

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { SideMenuBtnContextProvider } from './contexts/mobileSideMenuShow.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { FilteredPlantsContextProvider } from './contexts/filteredPlantsContext.jsx';
-
+import { CartContextProvider } from './contexts/CartContext.jsx';
 const queryClient = new QueryClient()
 
 
@@ -16,7 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <SideMenuBtnContextProvider>
           <FilteredPlantsContextProvider>
-            <App />
+            <CartContextProvider>
+              <App />
+            </CartContextProvider>
           </FilteredPlantsContextProvider>
         </SideMenuBtnContextProvider>
       </QueryClientProvider>

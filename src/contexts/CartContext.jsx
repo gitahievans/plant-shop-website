fbfc4f7c48f152c00ea9c9d79204853d359c4533
gitahievans <gitahievance@gmail.com>
2023-06-17 04:13:50 +0300

    Persist cart to localStorage

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,11 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
+const storedCart = JSON.parse(localStorage.getItem('cart') || []);
+
 export const CartContextProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(storedCart);
     const [cartTotalAmount, setCartTotalAmount] = useState(0);
     const [cartTotalQuantity, setCartTotalQuantity] = useState(0);
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart])
 
     return <CartContext.Provider value={{ cart, setCart, cartTotalAmount, setCartTotalAmount, cartTotalQuantity, setCartTotalQuantity }}>
         {children}
